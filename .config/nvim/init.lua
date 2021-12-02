f660fbd91a9fdd50ef093e9784cb21953cedc8dc
stew <36718502+myatktun@users.noreply.github.com>
2021-12-02 23:19:43 +0630

    Move keymaps & settings to init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,54 @@
-require('settings')
-require('keymaps')
+---------------------------- Keybinds --------------------------------
+local options = { noremap = true, silent = true}
+vim.api.nvim_set_keymap('n', '<Space>', '<NOP>', options)
+vim.g.mapleader= ' '
+
+vim.api.nvim_set_keymap('i', 'jj', '<Esc>', options)
+vim.api.nvim_set_keymap('n', '<leader>mv', ':mkview<CR>', options)
+vim.api.nvim_set_keymap('n', '<leader>lv', ':loadview<CR>', options)
+
+vim.api.nvim_set_keymap('n', '<leader>j', 'gT', options)
+vim.api.nvim_set_keymap('n', '<leader>k', 'gt', options)
+
+vim.api.nvim_set_keymap('n', '<leader>html', ':-1read $HOME/.config/nvim/.skeleton.html<CR>4jwf>a', options)
+
+vim.api.nvim_set_keymap('', '<C-t>', ':cd %:p:h<CR>:vs +te<CR>', options)
+vim.api.nvim_set_keymap('', '<leader><C-t>', ':cd %:p:h<CR>:sp +te<CR>', options)
+
+vim.api.nvim_set_keymap('t', '<Esc>', '<C-\\><C-n>', options)
+
+vim.api.nvim_set_keymap('n', '<C-n>', ':NvimTreeToggle<CR>', options)
+vim.api.nvim_set_keymap('n', '<leader>n', ':NvimTreeFocus<CR>', options)
+
+vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>h', options)
+vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>j', options)
+vim.api.nvim_set_keymap('n', '<C-k>', '<C-w>k', options)
+vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', options)
+
+
+vim.api.nvim_set_keymap('v', '<', '<gv', options)
+vim.api.nvim_set_keymap('v', '>', '>gv', options)
+
+---------------------------- Colorscheme --------------------------------
+
+vim.cmd[[colorscheme gruvbox]]
+
+---------------------------- Global Settings --------------------------------
+
+vim.o.cursorline = true
+vim.o.mouse = "a"
+vim.o.relativenumber = true
+vim.o.number = true
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+vim.o.expandtab = true
+vim.o.splitright = true
+vim.o.splitbelow = true
+vim.o.showcmd = true
+vim.o.termguicolors = true
+
+---------------------------- Plugins --------------------------------
+
 require('plugins')
 require('nv-cmp.init')
 require('nvim-lsp.init')
