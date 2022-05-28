aa8cc822ae34f13aae98e827c0e641c94ebf5645
Myat Kyaw Tun <myatktun2k@gmail.com>
2022-05-28 13:25:13 +0630

    Fixed auto detect audio output error
    audio output error occurred as order of pactl sinks changes every
    reboot. Fixed the issue by piping awk output to grep to select device,
    instead of using 'NR==1'
    
    i3/config     : set Mod1 (Alt) as $alt, bind keys to manually change
                    audio output
    i3/display.sh : fixed auto detect audio output error

--- .config/i3/display.sh
+++ .config/i3/display.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 INTERNAL_OUTPUT="eDP-1"
-INTERNAL_AUDIO=$(pactl list sinks | grep Name | awk -F: 'NR==1{print $2}')
+INTERNAL_AUDIO=$(pactl list sinks | grep Name | awk -F: '{print $2}' | grep analog)
 
 if [[ $(xrandr | grep HDMI-1 | grep -c ' connected ') -eq 1 ]]; then
   EXTERNAL_OUTPUT="HDMI-1"
   xrandr --output $INTERNAL_OUTPUT --off --output $EXTERNAL_OUTPUT --primary --mode 1920x1080 --refresh 120.40 --pos 0x0 --rotate normal
-  EXTERNAL_AUDIO=$(pactl list sinks | grep Name | awk -F: 'NR==2{print $2}')
+  EXTERNAL_AUDIO=$(pactl list sinks | grep Name | awk -F: '{print $2}' | grep hdmi)
   pactl set-default-sink $EXTERNAL_AUDIO
 else
   xrandr --output $INTERNAL_OUTPUT --primary --mode 1920x1080 --refresh 60.05 --pos 0x0 --rotate normal --output $EXTERNAL_OUTPUT --off
